8499a23b37bad46cc4dc03292abe2503727244c3
Andrei Bostan <abostan.ir@gmail.com>
2020-03-05 09:53:21 +0100

    started charts

--- src/model/TemperatureList.java
+++ src/model/TemperatureList.java
@@ -4,63 +4,80 @@ import java.util.ArrayList;
 
 public class TemperatureList
 {
-   private ArrayList<Temperature> list;
+  private ArrayList<Temperature> list;
 
-   public TemperatureList()
-   {
-      this.list = new ArrayList<>();
-   }
+  public TemperatureList()
+  {
+    this.list = new ArrayList<>();
+  }
 
-   public void addTemperature(Temperature temperature)
-   {
-      list.add(temperature);
-   }
+  public void addTemperature(Temperature temperature)
+  {
+    list.add(temperature);
+  }
 
-   public Temperature getTemperature(int index)
-   {
-      if (index >= 0 && index < list.size())
-      {
-         return list.get(index);
-      }
+  public Temperature getTemperature(int index)
+  {
+    if (index >= 0 && index < list.size())
+    {
+      return list.get(index);
+    }
+    return null;
+  }
+
+  public Temperature getLastTemperature(String id)
+  {
+    if (list.size() < 1)
+    {
       return null;
-   }
+    }
+    if (id == null)
+    {
+      return list.get(list.size() - 1);
+    }
+    for (int i = list.size() - 1; i >= 0; i--)
+    {
+      if (id.equals(list.get(i).getId()))
+      {
+        return list.get(i);
+      }
+    }
+    return null;
+  }
 
-   public Temperature getLastTemperature(String id)
-   {
-      if (list.size() < 1)
-      {
-         return null;
-      }
-      if (id == null)
-      {
-         return list.get(list.size()-1);
-      }
-      for (int i=list.size()-1; i>=0; i--)
-      {
-         if (id.equals(list.get(i).getId()))
-         {
-            return list.get(i);
-         }
-      }
-      return null;
-   }
+  public ArrayList<Temperature> getLastTemperatures(int number)
+  {
+     ArrayList<Temperature> newList = new ArrayList<>();
+    if (number > 20)
+    {
+       for (int i = list.size() - number; i < list.size(); i++)
+       {
+            newList.add(list.get(i));
+       }
+       return newList;
+    }
+    else
+    {
+       return list;
+    }
+  }
 
-   public int getSize()
-   {
-      return list.size();
-   }
+  public int getSize()
+  {
+    return list.size();
+  }
 
-   public String toString()
-   {
-      String s = "{";
-      for (int i = 0; i < list.size(); i++)
+  public String toString()
+  {
+    String s = "{";
+    for (int i = 0; i < list.size(); i++)
+    {
+      s += list.get(i);
+      if (i < list.size() - 1)
       {
-         s += list.get(i);
-         if (i < list.size() - 1)
-         {
-            s += ", ";
-         }
+        s += ", ";
       }
-      return s;
-   }
+    }
+    return s;
+  }
 }
--- src/view/TemperatureViewController.java
+++ src/view/TemperatureViewController.java
@@ -4,14 +4,15 @@ import javafx.fxml.FXML;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.layout.Region;
 import viewmodel.TemperatureViewModel;
 
 public class TemperatureViewController
 {
   @FXML private LineChart TemperatureChart;
-  @FXML private CategoryAxis TemperatureChartX;
-  @FXML private NumberAxis TemperatureChartY;
+//  @FXML private CategoryAxis TemperatureChartX;
+//  @FXML private NumberAxis TemperatureChartY;
 
   private Region root;
   private TemperatureViewModel viewModel;
@@ -25,6 +26,15 @@ public class TemperatureViewController
     this.viewHandler = viewHandler;
     this.viewModel = viewModel;
     this.root = root;
+
+    TemperatureChart.setTitle("HAI SA VADEM");
+    XYChart.Series series1 = new XYChart.Series();
+    series1.setName("Portfolio 1");
+
+    series1.getData().add(new XYChart.Data("Jan", 23));
+    series1.getData().add(new XYChart.Data("Feb", 14));
+    series1.getData().add(new XYChart.Data("Mar", 15));
+    TemperatureChart.getData().add(series1);
   }
 
   public void reset()
